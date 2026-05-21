8119c879fdc62cb375fd1f23ee9c77d39fc8f1df
wahaj248 <wahajkhan2488@gmail.com>
2026-05-21 22:07:58 +0500

    new

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  appType: "spa",
+  preview: {
+    port: 4173,
+  },
   server: {
     port: 5173,
     // proxy: {
